ed67a4d2ed3dc25bbe05fc7773c6c31c32206905
Siddhant Chole <cholesiddhant@gmail.com>
2026-06-19 15:28:36 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
+import java.io.File;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -25,7 +27,24 @@ public class Main {
 
                 if (builtins.contains(command)) {
                     System.out.println(command + " is a shell builtin");
-                } else {
+                    continue;
+                }
+
+                String pathEnv = System.getenv("PATH");
+                String[] paths = pathEnv.split(File.pathSeparator);
+
+                boolean found = false;
+
+                for (String dir : paths) {
+                    File file = new File(dir, command);
+
+                    if (file.exists() && file.canExecute()) {
+                        System.out.println(command + " is " + file.getAbsolutePath());
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
                     System.out.println(command + ": not found");
                 }
             }
